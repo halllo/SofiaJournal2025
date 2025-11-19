806495c079bbbcf62793b1083118ccd0fd89c71c
Manuel Naujoks <manuel.naujoks@stp.one>
2025-11-19 21:25:12 +0200

    aspire starts angular

--- AppHost1/AppHost.cs
+++ AppHost1/AppHost.cs
@@ -2,4 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.SofiaJournal2025_Backend>("SofiaJournal2025-Backend");
 
+builder.AddNpmApp("SofiaJournal2025-Frontend", "../sofia-journal-2025-ui")
+    .WithUrl("http://localhost:4200");
+
 builder.Build().Run();
